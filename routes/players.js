2d1d0014910e5c401b50e91b437a5705d3ce81dd
Netanel kalir <netanelkalir@gmail.com>
2025-07-15 18:16:51 +0300

    routs

---
+++ routes/players.js
@@ -0,0 +1,12 @@
+import express from "express";
+import {
+    getAllPlayers,
+    recordTime
+} from "../controllers/playerCtrl.js";
+
+const router = express.Router()//יצירת ראוטר שמקבל נתיבים מקומיים
+
+router.get("/getAllplayers", getAllPlayers)//שליפה של כל השחקנים
+router.put("/recordTime/:id", recordTime)//עדכון על ידי מזהה שחקן קיים
+
+export default router
---
+++ routes/riddles.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { 
+    getAllRiddles,
+    createRiddle,
+    updateriddle,
+    deleteriddle
+
+ } from "../controllers/riddleCtrl.js";
+
+ const router = express.Router()//יצירת ראוטר שמקבל נתיבים מקומיים
+
+ router.get("/getAllRiddle",getAllRiddles)//שליפה של כל החידות
+ router.post("/createRiddle",createRiddle)//יצירת חידה חדשה
+ router.put("/updateRiddle/:id",updateriddle)//עדכון על ידי מזהה חידה קיימת
+ router.delete("/deleteRiddle/:id",deleteriddle)//מחיקת על ידי מזהה חידה קייימת
+ 
+ 
+ export default router
